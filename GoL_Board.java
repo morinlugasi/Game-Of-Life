0b78057e3fe5a64508e57a2a63441361618ad730
Morin Ben Harush <103103157+morinlugasi@users.noreply.github.com>
2023-05-24 11:14:03 +0300

    Update GoL_Board.java

--- GoL_Board.java
+++ GoL_Board.java
@@ -16,7 +16,7 @@ public class GoL_Board extends CA
 	 * @param cols: Number of columns
 	 * Init new GoL automaton with an empty board
 	 */
-	private char [][] ; 
+	private char board [][] = new char[rows][cols] ; 
 	public GoL_Board(int rows, int cols)
 	{
 	
